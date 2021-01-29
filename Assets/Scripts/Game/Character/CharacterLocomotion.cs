bde7fa8c8ac05ce653a6e18ab123bb356cb25355
Eric Dou <lil.reenas@gmail.com>
2021-01-29 19:50:28 +0200

    Basic movement mechanic
    * added basic movement
    * added rigidbody to palyer
    * added player container script
    * added player locomotion script
    * added player settings scriptable object

---
+++ Assets/Scripts/Game/Character/CharacterLocomotion.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterLocomotion : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private CharacterContainer container = default;
+    [SerializeField] private CharacterSettings settings;
+    [SerializeField] private Rigidbody _rigidbody;
+    
+    
+    private readonly Vector3 gravity = Physics.gravity;
+    private Vector3 inputVelocity = default;
+    private Vector3 targetVelocity = default;
+    
+    #endregion
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rigidbody.freezeRotation = true;
+        _rigidbody.useGravity = false;
+        _rigidbody.angularDrag = 999;
+        _rigidbody.drag = 0;
+
+        // velocity = _rigidbody.velocity;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        inputVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+
+        // Transform directions from local to world space, for safety
+        targetVelocity = transform.TransformDirection(inputVelocity);
+        targetVelocity *= settings.MovementSpeed;
+
+        _rigidbody.velocity = targetVelocity;
+        
+        _rigidbody.AddForce(new Vector3(0, gravity.y * _rigidbody.mass, 0));
+    }
+}
---
+++ Assets/Scripts/Scriptable Objects/Source/CharacterSettings.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[System.Serializable, CreateAssetMenu(fileName = "CharacterSettings", menuName = "Character/CharacterSettings")]
+public class CharacterSettings : ScriptableObject
+{
+    public float MovementSpeed = 10f;
+}
